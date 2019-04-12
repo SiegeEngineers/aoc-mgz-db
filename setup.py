4d7cc4619c72797c41bdc158e2840b5be3a83ad0
happyleavesaoc <happyleaves.tfr@gmail.com>
2019-04-12 16:39:48 +0000

    update dependencies

--- setup.py
+++ setup.py
@@ -15,16 +15,16 @@ setup(
     ]},
     install_requires=[
         'alembic>=1.0.8',
-        'aocref',
-        'aocqq',
+        'aocref>=0.0.2',
+        'python-aoc-qq',
         'coloredlogs>=10.0',
         'iso8601>=0.1.12',
-        'mgz>=1.2.4',
+        'mgz>=1.2.5',
         'requests>=2.20.1',
         'requests-cache>=0.4.13',
         'SQLAlchemy>=1.2.14',
         'tqdm>=4.28.1',
-        'voobly>=1.2.6'
+        'voobly>=1.2.7'
     ],
     entry_points = {
         'console_scripts': ['mgzdb=mgzdb.__main__:setup'],
