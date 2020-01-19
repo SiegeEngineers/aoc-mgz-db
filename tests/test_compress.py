2e5874e7ebaa352b14e473310b92f1f6b16ed922
happyleavesaoc <happyleaves.tfr@gmail.com>
2020-01-19 14:50:46 +0000

    support aok compression

---
+++ tests/test_compress.py
@@ -0,0 +1,34 @@
+import io
+import struct
+import unittest
+import zlib
+
+from mgz.util import Version
+from mgzdb.compress import compress, decompress, compress_tiles, decompress_tiles
+
+
+class TestCompress(unittest.TestCase):
+
+    def test_compress(self):
+        header = zlib_header = zlib.compress(b'1' * 100)[2:]
+        body = b'0' * 100
+        rec = struct.pack('<II', len(header) + 8, 0) + header + body
+        compressed_rec = compress(io.BytesIO(rec))
+        self.assertEqual(rec, decompress(io.BytesIO(compressed_rec)))
+
+    def test_compress_aok(self):
+        header = zlib_header = zlib.compress(b'1' * 100)[2:]
+        body = b'0' * 100
+        rec = struct.pack('<I', len(header) + 4) + header + body
+        compressed_rec = compress(io.BytesIO(rec), version=Version.AOK)
+        self.assertEqual(rec, decompress(io.BytesIO(compressed_rec), version=Version.AOK))
+
+    def test_compress_tiles(self):
+        tiles = [
+            {'terrain_id': 1, 'elevation': 0, 'x': 0, 'y': 0},
+            {'terrain_id': 2, 'elevation': 1, 'x': 1, 'y': 0},
+            {'terrain_id': 1, 'elevation': 2, 'x': 0, 'y': 1},
+            {'terrain_id': 2, 'elevation': 3, 'x': 1, 'y': 1}
+        ]
+        compressed_tiles = compress_tiles(tiles)
+        self.assertEqual(tiles, decompress_tiles(compressed_tiles, 2))
